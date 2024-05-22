62b6e9d60d02c8efdbd6cee488d7c16eec8fbeba
João Victor Costa dos Santos <joaovictorcs29@gmail.com>
2024-05-22 17:58:18 -0300

    Product

--- src/main/java/org/example/course/resources/ProductResource.java
+++ src/main/java/org/example/course/resources/ProductResource.java
@@ -1,7 +1,7 @@
 package org.example.course.resources;
 
-import org.example.course.entities.Category;
-import org.example.course.services.CategoryService;
+import org.example.course.entities.Product;
+import org.example.course.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/categories")
-public class CategoryResource {
+@RequestMapping(value = "/products")
+public class ProductResource {
 
     @Autowired
-    private CategoryService service;
+    private ProductService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Category> findById(@PathVariable Long id){
-        Category obj = service.findById(id);
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+        Product obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
 }
